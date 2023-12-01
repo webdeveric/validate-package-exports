8e1e8f92f54769f8c07856265785cf26f6d4201c
Eric King <eric@webdeveric.com>
2023-12-01 16:52:55 -0600

    test: added tests for `assertIsPackageJson()`

---
+++ src/type-assertion.test.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect } from 'vitest';
+
+import { assertIsPackageJson } from './type-assertion.js';
+
+describe('assertIsPackageJson()', () => {
+  it('Throws when given invalid input', () => {
+    expect(() => {
+      assertIsPackageJson({});
+    }).toThrow();
+
+    expect(() => {
+      assertIsPackageJson({
+        name: 'package',
+        version: '1.1.1',
+      });
+    }).not.toThrow();
+  });
+});
