b87b32f590a2ab64164ff09230c143b4882cfe98
Eric King <eric@webdeveric.com>
2024-01-07 10:59:25 -0600

    chore: removed check for `parseArgs()`

--- src/cli.ts
+++ src/cli.ts
@@ -10,42 +10,38 @@ import { ExitCodes } from '@src/types.js';
 import { Validator } from './Validator.js';
 
 try {
-  if (typeof parseArgs === 'function') {
-    const args = parseArgs({
-      allowPositionals: true,
-      strict: true,
-      options: {
-        package: {
-          type: 'string',
-          short: 'p',
-          default: join(process.cwd(), 'package.json'),
-        },
-        info: {
-          type: 'boolean',
-          short: 'i',
-          default: false,
-        },
-        debug: {
-          type: 'boolean',
-          short: 'd',
-          default: process.env.RUNNER_DEBUG === '1',
-        },
+  const args = parseArgs({
+    allowPositionals: true,
+    strict: true,
+    options: {
+      package: {
+        type: 'string',
+        short: 'p',
+        default: join(process.cwd(), 'package.json'),
       },
-    });
+      info: {
+        type: 'boolean',
+        short: 'i',
+        default: false,
+      },
+      debug: {
+        type: 'boolean',
+        short: 'd',
+        default: process.env.RUNNER_DEBUG === '1',
+      },
+    },
+  });
 
-    await new Validator(
-      args.values,
-      new Console({
-        stdout: process.stdout,
-        stderr: process.stderr,
-        inspectOptions: {
-          depth: null,
-        },
-      }),
-    ).run();
-  } else {
-    console.warn('parseArgs is not a function. Please use Node JS >= 16.17');
-  }
+  await new Validator(
+    args.values,
+    new Console({
+      stdout: process.stdout,
+      stderr: process.stderr,
+      inspectOptions: {
+        depth: null,
+      },
+    }),
+  ).run();
 } catch (error) {
   if (error instanceof Error) {
     console.error(error.message);
