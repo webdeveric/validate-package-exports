4b971d4527ad79b9602e27759768cf2feec91e3d
Eric King <eric@webdeveric.com>
2024-03-24 22:50:09 -0500

    chore: removed unused code

--- src/lib/Task.ts
+++
@@ -1,7 +0,0 @@
-import type { TaskRunContext, TaskResult } from '@src/types.js';
-
-export abstract class Task {
-  abstract name: string;
-
-  abstract run(context: TaskRunContext): Promise<TaskResult>;
-}
