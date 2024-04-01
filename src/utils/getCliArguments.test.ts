64cd78bb64e90a1147f4a499ad792c8f9f13ea32
Eric King <eric@webdeveric.com>
2024-04-01 17:01:29 -0500

    test: added tests

---
+++ src/utils/getCliArguments.test.ts
@@ -0,0 +1,57 @@
+import { availableParallelism } from 'node:os';
+
+import { describe, expect, it } from 'vitest';
+
+import { getCliArguments } from './getCliArguments.js';
+
+describe('getCliArguments()', () => {
+  it('Returns CliArguments', () => {
+    expect(getCliArguments([])).toEqual({
+      bail: false,
+      check: false,
+      info: false,
+      json: false,
+      verify: false,
+      concurrency: availableParallelism(),
+      packages: ['./package.json'],
+    });
+
+    expect(
+      getCliArguments([
+        './some-path/package.json',
+        '--bail',
+        '--info',
+        '--check',
+        '--verify',
+        '--json',
+        '--concurrency=1',
+      ]),
+    ).toEqual({
+      bail: true,
+      check: true,
+      info: true,
+      json: true,
+      verify: true,
+      concurrency: 1,
+      packages: ['./some-path/package.json'],
+    });
+
+    expect(getCliArguments(['--bail', '--no-bail'])).toEqual(
+      expect.objectContaining({
+        bail: false,
+      }),
+    );
+
+    expect(getCliArguments(['--info', '--no-info'])).toEqual(
+      expect.objectContaining({
+        info: false,
+      }),
+    );
+  });
+
+  it('Throws when given incorrect arguments', () => {
+    expect(() => {
+      getCliArguments(['--not-a-real-flag']);
+    }).toThrow();
+  });
+});
