9619084008f3bc4e28b18b4b3d410a2a815eb45b
Eric King <eric@webdeveric.com>
2024-04-01 17:39:29 -0500

    test: added tests

---
+++ src/lib/Result.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from 'vitest';
+
+import { Result, ResultCode } from './Result.js';
+
+describe('Result', () => {
+  it('Holds data describing what happened', () => {
+    const result = new Result({
+      name: 'file-exists',
+      code: ResultCode.Success,
+      message: 'message',
+      entryPoint: {
+        packagePath: 'package.json',
+        moduleName: undefined,
+        type: 'module',
+        fileName: 'index.js',
+        relativePath: './index.js',
+        directory: '/tmp',
+        resolvedPath: '/tmp/index.js',
+        subpath: undefined,
+        condition: undefined,
+        itemPath: ['main'],
+      },
+    });
+
+    expect(result.toString()).toEqual('✅ file-exists: message');
+
+    result.code = ResultCode.Error;
+
+    expect(result.toString()).toEqual('❌ file-exists: message (["main"])');
+
+    result.code = ResultCode.Skip;
+
+    expect(result.toString()).toEqual('😐 file-exists: message');
+  });
+});
