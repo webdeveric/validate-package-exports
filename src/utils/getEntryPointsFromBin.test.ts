ed9df9ed5c3569f8fd39329d31fb4f6ec5dc9a0d
Eric King <eric@webdeveric.com>
2024-04-02 19:19:40 -0500

    test: resolve paths to work on Windows

--- src/utils/getEntryPointsFromBin.test.ts
+++ src/utils/getEntryPointsFromBin.test.ts
@@ -1,3 +1,5 @@
+import { resolve } from 'node:path';
+
 import { describe, expect, it } from 'vitest';
 
 import { getEntryPointsFromBin } from './getEntryPointsFromBin.js';
@@ -23,13 +25,13 @@ describe('getEntryPointsFromBin()', () => {
     ).toEqual([
       {
         condition: undefined,
-        directory: '/tmp',
+        directory: resolve('/tmp'),
         fileName: 'bin.js',
         itemPath: ['bin'],
         moduleName: undefined,
         packagePath: './package.json',
         relativePath: 'bin.js',
-        resolvedPath: '/tmp/bin.js',
+        resolvedPath: resolve('/tmp/bin.js'),
         subpath: undefined,
         type: 'module',
       },
@@ -50,32 +52,32 @@ describe('getEntryPointsFromBin()', () => {
             name: 'test',
             type: 'module',
             path: './package.json',
-            directory: '/tmp',
+            directory: resolve('/tmp'),
           },
         ),
       ),
     ).toEqual([
       {
         condition: undefined,
-        directory: '/tmp',
+        directory: resolve('/tmp'),
         fileName: 'tool.js',
         itemPath: ['bin', 'tool'],
         moduleName: undefined,
         packagePath: './package.json',
         relativePath: 'tool.js',
-        resolvedPath: '/tmp/tool.js',
+        resolvedPath: resolve('/tmp/tool.js'),
         subpath: undefined,
         type: 'module',
       },
       {
         condition: undefined,
-        directory: '/tmp',
+        directory: resolve('/tmp'),
         fileName: 'example.js',
         itemPath: ['bin', 'example'],
         moduleName: undefined,
         packagePath: './package.json',
         relativePath: 'example.js',
-        resolvedPath: '/tmp/example.js',
+        resolvedPath: resolve('/tmp/example.js'),
         subpath: undefined,
         type: 'module',
       },
