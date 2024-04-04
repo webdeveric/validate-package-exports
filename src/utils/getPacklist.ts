66207c682a92be9299b0709fba7e06f46601ee2a
Eric King <eric@webdeveric.com>
2024-04-04 09:18:12 -0500

    chore: added `getPacklist()`

---
+++ src/utils/getPacklist.ts
@@ -0,0 +1,11 @@
+export async function getPacklist(directory: string): Promise<string[]> {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const Arborist = (await import('@npmcli/arborist')).default;
+  const packlist = (await import('npm-packlist')).default;
+
+  const arborist = new Arborist({ path: directory });
+  const tree = await arborist.loadActual();
+  const files = await packlist(tree);
+
+  return files;
+}
