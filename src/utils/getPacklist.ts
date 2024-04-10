874461601f6b6025a09e1d54849840afa859e6d1
Eric King <eric@webdeveric.com>
2024-04-10 15:25:30 -0500

    chore: moved imports up

--- src/utils/getPacklist.ts
+++ src/utils/getPacklist.ts
@@ -1,10 +1,10 @@
-import { fixSlash } from './fixSlash.js';
+// eslint-disable-next-line @typescript-eslint/naming-convention, import/no-named-as-default
+import Arborist from '@npmcli/arborist';
+import packlist from 'npm-packlist';
+
+import { fixSlash } from '@utils/fixSlash.js';
 
 export async function getPacklist(directory: string): Promise<string[]> {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  const Arborist = (await import('@npmcli/arborist')).default;
-  const packlist = (await import('npm-packlist')).default;
-
   const arborist = new Arborist({ path: directory });
   const tree = await arborist.loadActual();
   const files = await packlist(tree);
