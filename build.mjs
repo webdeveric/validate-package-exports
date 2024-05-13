a75a29096c1a92f42eefdc2d0a259eb821fc46af
Eric King <eric@webdeveric.com>
2024-05-12 19:54:06 -0500

    fix: updated build script to account for `assert`/`with` when importing json.

--- build.mjs
+++ build.mjs
@@ -5,7 +5,11 @@ import { build } from 'esbuild';
 import { clean } from 'esbuild-plugin-clean';
 import { environmentPlugin } from 'esbuild-plugin-environment';
 
-import pkg from './package.json' assert { type: 'json' };
+const nodeVersion = Number.parseInt(String(process.versions.node.split('.').at(0)));
+
+const { default: pkg } = await import('./package.json', {
+  [nodeVersion < 20 ? 'assert' : 'with']: { type: 'json' },
+});
 
 try {
   const results = await build({
