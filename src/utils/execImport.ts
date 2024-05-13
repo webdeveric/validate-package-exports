633e3b5fd9dd6b9a100f4e2c48e3ed552e99001f
Eric King <eric@webdeveric.com>
2024-05-12 19:44:06 -0500

    fix: use `with` instead of `assert` when using Node 20+

--- src/utils/execImport.ts
+++ src/utils/execImport.ts
@@ -1,10 +1,13 @@
 import { asyncExec } from './asyncExec.js';
+import { getNodeMajorVersion } from './getNodeMajorVersion.js';
 
 import type { ExecOptions } from 'node:child_process';
 
+const nodeVersion = getNodeMajorVersion();
+
 export async function execImport(path: string, options: ExecOptions): Promise<void> {
   const command = /\.json$/i.test(path)
-    ? `node --input-type=module --eval="import '${path}' assert { type: 'json' };"`
+    ? `node --input-type=module --eval="import '${path}' ${nodeVersion < 20 ? 'assert' : 'with'} { type: 'json' };" --no-warnings`
     : `node --input-type=module --eval="import '${path}';"`;
 
   await asyncExec(command, options);
