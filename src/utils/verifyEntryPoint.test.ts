3c91215ccea5e361ec9cbeec4e56b3355f809595
Eric King <eric@webdeveric.com>
2024-07-27 15:02:00 -0500

    test: added tests for `shouldRequire()` and `shouldImport()`

---
+++ src/utils/verifyEntryPoint.test.ts
@@ -0,0 +1,135 @@
+import { describe, expect, it } from 'vitest';
+
+import { shouldRequire, shouldImport } from './verifyEntryPoint.js';
+
+describe('shouldRequire()', () => {
+  it('Returns false for esm packages', () => {
+    expect(
+      shouldRequire({
+        packagePath: './package.json',
+        moduleName: 'example-package',
+        type: 'module',
+        fileName: 'test.js',
+        relativePath: 'test.js',
+        directory: process.cwd(),
+        resolvedPath: process.cwd(),
+        subpath: undefined,
+        condition: undefined,
+        itemPath: ['main'],
+      }),
+    ).toBeFalsy();
+  });
+
+  it('Returns a boolean', () => {
+    expect(
+      shouldRequire({
+        packagePath: './package.json',
+        moduleName: 'example-package',
+        type: 'commonjs',
+        fileName: 'test.js',
+        relativePath: 'test.js',
+        directory: process.cwd(),
+        resolvedPath: process.cwd(),
+        subpath: undefined,
+        condition: undefined,
+        itemPath: ['main'],
+      }),
+    ).toBeTruthy();
+
+    expect(
+      shouldRequire({
+        packagePath: './package.json',
+        moduleName: 'example-package',
+        type: 'module',
+        fileName: 'test.js',
+        relativePath: 'test.js',
+        directory: process.cwd(),
+        resolvedPath: process.cwd(),
+        subpath: '.',
+        condition: 'require',
+        itemPath: ['exports', '.', 'require'],
+      }),
+    ).toBeTruthy();
+
+    expect(
+      shouldRequire({
+        packagePath: './package.json',
+        moduleName: 'example-package',
+        type: 'module',
+        fileName: 'test.js',
+        relativePath: 'test.js',
+        directory: process.cwd(),
+        resolvedPath: process.cwd(),
+        subpath: '.',
+        condition: 'import',
+        itemPath: ['exports', '.', 'import'],
+      }),
+    ).toBeFalsy();
+  });
+});
+
+describe('shouldImport()', () => {
+  it('Returns true for cjs packages', () => {
+    expect(
+      shouldImport({
+        packagePath: './package.json',
+        moduleName: 'example-package',
+        type: 'commonjs',
+        fileName: 'test.js',
+        relativePath: 'test.js',
+        directory: process.cwd(),
+        resolvedPath: process.cwd(),
+        subpath: undefined,
+        condition: undefined,
+        itemPath: ['main'],
+      }),
+    ).toBeTruthy();
+  });
+
+  it('Returns a boolean', () => {
+    expect(
+      shouldImport({
+        packagePath: './package.json',
+        moduleName: 'example-package',
+        type: 'module',
+        fileName: 'test.js',
+        relativePath: 'test.js',
+        directory: process.cwd(),
+        resolvedPath: process.cwd(),
+        subpath: undefined,
+        condition: undefined,
+        itemPath: ['main'],
+      }),
+    ).toBeTruthy();
+
+    expect(
+      shouldImport({
+        packagePath: './package.json',
+        moduleName: 'example-package',
+        type: 'commonjs',
+        fileName: 'test.js',
+        relativePath: 'test.js',
+        directory: process.cwd(),
+        resolvedPath: process.cwd(),
+        subpath: '.',
+        condition: 'import',
+        itemPath: ['exports', '.', 'import'],
+      }),
+    ).toBeTruthy();
+
+    expect(
+      shouldImport({
+        packagePath: './package.json',
+        moduleName: 'example-package',
+        type: 'module',
+        fileName: 'test.js',
+        relativePath: 'test.js',
+        directory: process.cwd(),
+        resolvedPath: process.cwd(),
+        subpath: '.',
+        condition: 'import',
+        itemPath: ['exports', '.', 'import'],
+      }),
+    ).toBeTruthy();
+  });
+});
