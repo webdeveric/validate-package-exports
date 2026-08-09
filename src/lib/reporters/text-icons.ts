5a7d9e538c685b8c78064e92a0976f3ac7a62425
Eric King <eric@webdeveric.com>
2026-08-09 10:45:44 -0500

    feat: add additional text icons with corresponding Unicode values

--- src/lib/reporters/text-icons.ts
+++ src/lib/reporters/text-icons.ts
@@ -1,15 +1,74 @@
+/**
+ * Check mark - `0x2713`
+ */
 export const checkMark = '✓';
 
-export const xMark = '✗';
+/**
+ * Ballot X - `0x2717`
+ */
+export const ballotX = '✗';
 
-export const emptyBox = '□';
+/**
+ * Warning - `0x27A0`
+ */
+export const warning = '⚠';
 
+/**
+ * Circled division slash - `0x2298`
+ */
+export const circledDivisionSlash = '⊘';
+
+/**
+ * White square - `0x25A1`
+ */
+export const whiteSquare = '□';
+
+/**
+ * White circle - `0x25CB`
+ */
+export const whiteCircle = '○';
+
+/**
+ * Black circle - `0x25CF`
+ */
+export const blackCircle = '●';
+
+/**
+ * Leftwards arrow - `0x2190`
+ */
 export const leftArrow = '←';
 
+/**
+ * Rightwards arrow - `0x2192`
+ */
 export const rightArrow = '→';
 
+/**
+ * Upwards arrow - `0x2191`
+ */
 export const upArrow = '↑';
 
+/**
+ * Downwards arrow - `0x2193`
+ */
 export const downArrow = '↓';
 
-export const pencil = '✎';
+/**
+ * Horizontal ellipsis - `0x2026`
+ */
+export const horizontalEllipsis = '…';
+
+/**
+ * Arrow pointing downwards then curving rightwards - `0x2937`
+ */
+export const curvingRightArrow = '⤷';
+
+/**
+ * Package - `0x1F4E6`
+ */
+export const packageBox = '📦';
+
+/**
+ * Right-pointing magnifying glass - `0x1F50E`
+ */
+export const magnifyingGlass = '🔎';
